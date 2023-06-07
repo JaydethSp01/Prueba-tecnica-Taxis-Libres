7abe43f60702f55d146c42ef30753a7ca0a7f1fd
JaydethSp01 <jsimarrapolo@gmail.com>
2023-06-07 13:16:33 -0500

    pagina terminada

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // nav en todo momento
 window.addEventListener('scroll', function() {
     var nav = document.getElementById('navbar');
@@ -7,6 +8,16 @@ window.addEventListener('scroll', function() {
         nav.classList.remove('scrolled');
     }
 });
+
+// efecto de carga
+window.addEventListener('load', () =>{
+  const preloader = document.querySelector(".preloader");
+  preloader.style.opacity = 0;
+  preloader.style.visibility = "hidden";
+
+  
+}); 
+
 
 //menu despegable
 
@@ -89,6 +100,274 @@ tipoInscripcion.addEventListener("change", function() {
   }
 });
 
+//validación de formulario
 
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const tipoInscripcion = document.getElementById("tipo_inscripcion").value;
+  const nombre = document.getElementById("nombre").value;
+  const nombreGrupo = document.getElementById("nombre_grupo").value;
+  const numeroParticipantes = document.getElementById("numero_participantes").value;
+  const nombreStartup = document.getElementById("nombre_startup").value;
+  const numeroParticipantesStartup = document.getElementById("numero_participantes_startup").value;
+  const email = document.getElementById("email").value;
+  const fechaNacimiento = document.getElementById("fecha_nacimiento").value;
+  const fechaExpedicion = document.getElementById("fecha_expedicion").value;
+  const especializacion = document.getElementById("especializacion").value;
+  const reto = document.getElementById("reto").value;
+  const ciudad = document.getElementById("ciudad").value;
+  const tipoDocumento = document.getElementById("tipo_documento").value;
+  const numeroDocumento =document.getElementById("numero_documento").value;
+
+  let valid = true;
+
+
+  const regexNombre = /^[A-Za-z\s]+$/;
+
+  if (tipoInscripcion === "") {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor, selecciona el tipo de inscripción",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  }
+
+  if (tipoInscripcion === "Individual" && nombre === "") {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor, ingresa tu nombre",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  } else if (tipoInscripcion === "Individual" && !regexNombre.test(nombre)) {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "El nombre solo puede contener letras y espacios",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  }
+
+  if (tipoInscripcion === "Grupo") {
+    if (nombreGrupo === "") {
+      valid = false;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Por favor, ingresa el nombre del grupo",
+        footer: "Haz clic en el botón para cerrar",
+        timer: 5000,
+        timerProgressBar: true
+      });
+    }
+    if (numeroParticipantes === "") {
+      valid = false;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Por favor, ingresa el número de participantes (de 1 a 5)",
+        footer: "Haz clic en el botón para cerrar",
+        timer: 5000,
+        timerProgressBar: true
+      });
+    } else if (numeroParticipantes < 1 || numeroParticipantes >= 5) {
+      valid = false;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "El número de participantes debe ser de 1 a 5",
+        footer: "Haz clic en el botón para cerrar",
+        timer: 5000,
+        timerProgressBar: true
+      });
+    }
+  }
+
+  
+  if (tipoInscripcion === "Startup") {
+    if (nombreStartup === "") {
+      valid = false;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Por favor, ingresa el nombre de la startup o emprendimiento",
+        footer: "Haz clic en el botón para cerrar",
+        timer: 5000,
+        timerProgressBar: true
+      });
+    }
+    if (numeroParticipantesStartup === "") {
+      valid = false;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Por favor, ingresa el número de participantes (de 3 a 5)",
+        footer: "Haz clic en el botón para cerrar",
+        timer: 5000,
+        timerProgressBar: true
+      });
+    } else if (numeroParticipantesStartup < 1 || numeroParticipantesStartup >= 5) {
+      valid = false;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "El número de participantes debe ser de 1 a 5",
+        footer: "Haz clic en el botón para cerrar",
+        timer: 5000,
+        timerProgressBar: true
+      });
+    }
+  }
+
+ 
+  if (email === "") {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor, ingresa tu correo electrónico",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  } else if (!validateEmail(email)) {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor, ingresa un correo electrónico válido",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  }
+  if (fechaNacimiento === "") {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor, ingresa tu fecha de nacimiento",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  } else if (!isAdult(fechaNacimiento)) {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Debes ser mayor de edad para participar en el Hackathon",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  }
+
+  // Validar fecha de expedición
+  if (fechaExpedicion === "") {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor, ingresa la fecha de expedición del documento",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  }
+
+  if (tipoInscripcion === "" || nombre === "" || email === "" || ciudad === "" || tipoDocumento === "" || numeroDocumento === "" || fechaNacimiento === "" || fechaExpedicion === "" || especializacion === "" || reto === "") {
+    valid = false;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Por favor, completa todos los campos antes de enviar el formulario",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 5000,
+      timerProgressBar: true
+    });
+  } else if (valid) {
+    Swal.fire({
+      icon: "success",
+      title: "¡Inscripción exitosa!",
+      text: "Te has inscrito correctamente en el Hackathon. Pronto recibirás un correo con los pasos a seguir.",
+      footer: "Haz clic en el botón para cerrar",
+      timer: 8000,
+      timerProgressBar: true
+    });
+  }
+  
+
+  }
+);
+
+
+function validateEmail(email) {
+  const regexEmail = /\S+@\S+\.\S+/;
+  return regexEmail.test(email);
+}
+
+
+function isAdult(fechaNacimiento) {
+  const hoy = new Date();
+  const nacimiento = new Date(fechaNacimiento);
+  const edad = hoy.getFullYear() - nacimiento.getFullYear();
+  const diferenciaMeses = hoy.getMonth() - nacimiento.getMonth();
+  if (diferenciaMeses < 0 || (diferenciaMeses === 0 && hoy.getDate() < nacimiento.getDate())) {
+    edad--;
+  }
+  return edad >= 18;
+}
+
+//efecto de scrool
+window.sr = ScrollReveal();
+ sr.reveal(' .texto-header' , {
+    duration:4000,
+    origin:'bottom',
+    distance:'-150px'
+ })
+ sr.reveal(' .container-1' , {
+    duration:4000,
+    origin:'left',
+    distance:'400px'
+ })
+ sr.reveal(' .galeria-cont' , {
+  duration:4000,
+  origin:'bottom',
+  distance:'-150px'
+})
+sr.reveal(' .info' , {
+  duration:4000,
+  origin:'left',
+  distance:'400px'
+})
+sr.reveal(' .texto-elemento' , {
+  duration:4000,
+  origin:'left',
+  distance:'400px'
+})
+sr.reveal(' .titulo-scrool' , {
+  duration:4000,
+  origin:'bottom',
+  distance:'-100px'
+})
+sr.reveal(' .form' , {
+  duration:3000,
+  origin:'left',
+  distance:'400px'
+})
 
 
